9650076bd2001c2afd812ffc83ba84849421475d
Khalilullah Baig <108619223+KhalilBaig@users.noreply.github.com>
2025-08-25 23:58:36 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import './index.css'   // 👈 yahan apni css import karo
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/EduPanel-Student-Portal-UI/">
+  <HashRouter basename="/EduPanel-Student-Portal-UI/">
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
